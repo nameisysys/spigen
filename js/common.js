72d57a836bccd80f9ab0556e420b173c18bde055
nameisysys <ysjeon@ebrain.kr>
2016-11-24 12:42:11 +0900

    ssd

--- js/common.js
+++ js/common.js
@@ -81,7 +81,7 @@ $(document).ready(function () {
     });
 });
 
-//Facebook
+//Facebook Comment
 window.fbAsyncInit = function () {
     FB.init({
         appId: '607233349484734',
@@ -99,6 +99,41 @@ window.fbAsyncInit = function () {
     js.src = "//connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+//Facebook Script Loader + Page Tracking
+! function (f, b, e, v, n, t, s) {
+    if (f.fbq) return;
+    n = f.fbq = function () {
+        n.callMethod ?
+            n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = !0;
+    n.version = '2.0';
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = !0;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s)
+}(window,
+    document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '1046344992096861');
+fbq('track', 'PageView');
+
+function track(_pid, _aid, _eid) {
+    fbq('trackCustom', 'amz', {
+        pid: _pid, // page id
+        aid: _aid, // (optional) asin
+        eid: _eid // event id
+    });
+}
+
+function trk(_pid, _eid) {
+    return track(_pid, 'B01DU55KX8', _eid);
+}
 
 //GA
 (function (i, s, o, g, r, a, m) {
